10b36cd243394b7e4c173a5b70a38e5762ddd876
Thierno Diallo <63030553+Thierno-M-Diallo@users.noreply.github.com>
2024-02-21 23:33:52 -0500

    Create Home_Page main.js

---
+++ MCGardens/Home_Page main.js
@@ -0,0 +1,25 @@
+const http = require('node:http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  // Read the HTML file containing the webpage structure
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end('Error loading the file');
+      return;
+    }
+    res.end(data);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
