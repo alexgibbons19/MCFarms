042fcc1f4287c55c91fccdf9dabc1d1413deb3b6
kaweckil <80421756+kaweckil@users.noreply.github.com>
2024-04-09 14:31:10 -0400

    Update CalendarPage.jsx

--- MCGardens/src/CalendarPage.jsx
+++ MCGardens/src/CalendarPage.jsx
@@ -1,65 +1,89 @@
 import React, {useState} from 'react';
 import BurgerMenu from './BurgerMenu.jsx';
-import './assets/Calendar.css';
+import {Calendar,momentLocalizer} from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+//customized styles
+import './assets/CalendarPage.css';
 
-const Calendar = () => {
+
+// moment localization
+// default to current date and month and year
+moment.locale('en');
+const localizer = momentLocalizer(moment);
+
+//testing
+const events = [
+  {
+    title: 'Watering 1',
+    start: new Date(2024, 3, 10, 0, 0),
+    end: new Date(2024, 3, 10, 24, 0),
+    data:{
+      type:"watering"
+    }
+  },
+  {
+    title: 'Thunderstorm',
+    start: new Date(2024, 3, 15, 11, 0),
+    end: new Date(2024, 3, 15, 15, 0),
+    data:{
+      type:"weather",
+    }
+  },
+  {
+    title: 'Harvest date',
+    start: new Date(2024, 3, 30, 0, 0),
+    end: new Date(2024, 3, 30, 24, 0),
+  },
+];
+
+const components = {
+  event: (props,any) => {
+  const eventType = props?.event?.data?.type;
+    switch (eventType){
+      case "watering":
+        return(
+          <div style={{background:"blue",color:"white"}}>
+            {props.title}
+          </div>
+        );
+      case "weather":
+        return(
+          <div style={{background:"red",color:"white",font:"bold"}}>
+            WEATHER WARNING : <br />
+            {props.title}
+          </div>
+        );
+      default:
+        return(
+          <div style={{background:"green",color:"white"}}>
+            {props.title}
+          </div>
+        )    
+    }
+  },
+};
+
+const CalendarPage = () => {
+  //const [selectEvent,setSelectedEvent]=useState(null);
+  //const [editEvent,]
 
     return(
     <>
     <BurgerMenu />
-    <div className="calendar-container">
-      <h1>User's Calendar</h1>
-      <div className="month-banner"></div>
-      <div className = "weekdays-container">
-      <div className = "weekday">
-        <h1>Sunday</h1></div>
-      <div className = "weekday">
-        <h1>Monday</h1></div>
-      <div className = "weekday">
-        <h1>Tuesday</h1></div>
-      <div className = "weekday">
-        <h1>Wednesday</h1></div>
-      <div className = "weekday">
-        <h1>Thursday</h1></div>
-      <div className = "weekday">
-        <h1>Friday</h1></div>
-      <div className = "weekday">
-        <h1>Saturday</h1></div>
-      </div>
-      <div className = "day-grid-container"/*these need data from reminders api*/>
-      <div className="day">1</div>
-      <div className="day">2</div>
-      <div className="day">3</div>
-      <div className="day">4</div>
-      <div className="day">5</div>
-      <div className="day">6</div>
-      <div className="day">7</div>
-      <div className="day">8</div>
-      <div className="day">9</div>
-      <div className="day">10</div>
-      <div className="day">11</div>
-      <div className="day">12</div>
-      <div className="day">13</div>
-      <div className="day">14</div>
-      <div className="day">15</div>
-      <div className="day">16</div>
-      <div className="day">17</div>
-      <div className="day">18</div>
-      <div className="day">19</div>
-      <div className="day">20</div>
-      <div className="day">21</div>
-      <div className="day">22</div>
-      <div className="day">23</div>
-      <div className="day">24</div>
-      <div className="day">25</div>
-      <div className="day">26</div>
-      <div className="day">27</div>
-      <div className="day">28</div>
-      </div>
+    <div style={{height: "100vh", width:"150vh",padding:"10px"}}>
+    <Calendar
+      localizer={localizer}
+      events={events}
+      components={components}
+      startAccessor="start"
+      endAccessor="end"
+      defaultDate={new Date()} //current date
+    />
     </div>
     </>
   );
 }
 
 
-export default Calendar
+export default CalendarPage;
