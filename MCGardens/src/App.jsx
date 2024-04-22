511259c89a10c141c9aaae24851fbe27dce54b1f
Faddi Azzam Dabain <dottycr02@gmail.com>
2024-04-22 16:33:29 -0400

    fix css of burger menu and plant details

--- MCGardens/src/App.jsx
+++ MCGardens/src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import DiscussionBoard from './DiscussionBoard'
 import OptimalPlants from './OptimalPlants'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Thread from './Thread';
 import HomePage from './HomePage';
 import CalendarPage from './CalendarPage';
 import Reminders from './RemindersPage';
 import Login from './Login.jsx';
 import Weather from './Weather.jsx';
 import PlantDetails from './PlantDetails.jsx';
-import SearchPage from './SearchPage.jsx';
 import SignUp from './SignUp.jsx';
 import ForgotPassword from './ForgotPassword.jsx';
 
@@ -23,13 +21,11 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/home-page" element={<HomePage />}/>
         <Route path="/discussion-board" element={<DiscussionBoard />} />
-        <Route path="/thread/:id" element={<Thread/>} />
         <Route path="/optimal-plants" element={<OptimalPlants />} />
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/reminders" element={<Reminders />} />
         <Route path="/weather" element={<Weather />} />
         <Route path="/plant-details" element={<PlantDetails />} />
-        <Route path="/search-page" element={<SearchPage />} />
         {/* <Route path="/inventory" element={<InventoryPage />} />  */}
       </Routes>
     </Router>      
