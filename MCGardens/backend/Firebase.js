dcc4458d1f6e5b58bcc121b37b34d9c252921899
Faddi Azzam Dabain <dottycr02@gmail.com>
2024-04-23 21:17:01 -0400

    finally found a way to hide the firebase secrets

--- MCGardens/backend/Firebase.js
+++ MCGardens/backend/Firebase.js
@@ -22,14 +22,14 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBdBh6oBgnUZJ6dROPNOp5Wwxyvrr8epLQ",
-    authDomain: "mcgardens-bd0b1.firebaseapp.com",
-    databaseURL: "https://mcgardens-bd0b1-default-rtdb.firebaseio.com",
-    projectId: "mcgardens-bd0b1",
-    storageBucket: "mcgardens-bd0b1.appspot.com",
-    messagingSenderId: "102086093090",
-    appId: "1:102086093090:web:f8c4183ccf4594d6eedd7b",
-    measurementId: "G-G642QLL9KX"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
