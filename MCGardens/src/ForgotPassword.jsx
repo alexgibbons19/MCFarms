5c73e2d136705ce820e1a5a517d97ba8189b6549
gibbbeh <alexaderg@gmail.com>
2024-04-25 15:04:35 -0400

    Added forgot password

--- MCGardens/src/ForgotPassword.jsx
+++ MCGardens/src/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './assets/Login.css';
 import { resetPassword } from '../backend/Firebase';
 import { useNavigate } from "react-router-dom";
@@ -9,6 +9,14 @@ function ForgotPassword() {
   const [error, setError] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
+
+  useEffect(() => {
+    if (successMessage) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000); // Redirect after 3 seconds (adjust as needed)
+    }
+  }, [successMessage, navigate]);
   const handleChange = (e) => {
     setEmail(e.target.value);
     setError('');
